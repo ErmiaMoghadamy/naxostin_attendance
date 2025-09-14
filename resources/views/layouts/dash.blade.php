b03d83289ab37b29db7b6006b87fd1d7b87ed5be
Ermia <ermia.codev@gmail.com>
2025-09-14 22:44:32 +0330

    title fix #1

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -279,7 +279,7 @@
     >
       <a
         class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white"
-        href="#"
+        href="{{ route('dash.main') }}"
         >NaXostin</a
       >
       <ul class="navbar-nav flex-row d-md-none">

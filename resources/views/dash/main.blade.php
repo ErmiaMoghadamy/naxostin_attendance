010bb24625a097c5125b8615b2d752af29c72d4e
Ermia <ermia.codev@gmail.com>
2025-09-15 12:55:42 +0330

    add header

--- resources/views/dash/main.blade.php
+++ resources/views/dash/main.blade.php
@@ -3,6 +3,38 @@
 @section('content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Dashboard</h1>
+</div>
 
+<div class="row">
+    @if (auth()->user()->role == "admin")
+        <div class="col-12 col-md-3 p-3">
+            <a href="{{ route('dash.admin.teachers.index') }}" class="underline-none text-decoration-none">
+                <div class="text-light bg-dark px-3 pt-3 pb-1 rounded border">
+                    <h2 class="mb-">Teachers</h2>
+                    <p class="py-2"></p>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-12 col-md-3 p-3">
+            <a href="{{ route('dash.admin.teachers.index') }}" class="underline-none text-decoration-none">
+                <div class="text-light bg-dark px-3 pt-3 pb-1 rounded border">
+                    <h2 class="mb-">Classbooks</h2>
+                    <p class="py-2"></p>
+                </div>
+            </a>
+        </div>
+
+    @elseif (auth()->user()->role == "teacher")
+        <div class="col-12 col-md-3 p-3">
+            <a href="{{ route('dash.teacher.classbooks.index') }}" class="underline-none text-decoration-none">
+                <div class="text-light bg-dark px-3 pt-3 pb-1 rounded border">
+                    <h2 class="mb-">My Classbooks</h2>
+                    <p class="py-2"></p>
+                </div>
+            </a>
+        </div>
+    @endif
+  
 </div>
 @stop
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="light">
 
-        <title> NaXostin Attendace </title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> NaXostin Attendace </title>
+    <script src="/assets/js/color-modes.js"></script>
+    <meta name="theme-color" content="#712cf9" />
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/dashboard.css"  />
+</head>
 
-        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-    </head>
-    <body>
-        <div class="container">
-            @yield(section: 'content')
-        </div>
-        <script src="/assets/js/bootstrap.min.js"></script>
-    </body>
+<body>
+    <x-toggle-theme />
+
+    <div class="fixed-top p-3 bg-dark text-light shadow">
+        <h2 class="text-center fs-3">
+            NaXostin Attendace
+        </h2>
+    </div>
+    <div class="container">
+        @yield(section: 'content')
+    </div>
+    <script src="/assets/js/bootstrap.bundle.js"></script>
+    <script src="/assets/js/dashboard.js"></script>
+
+</body>
+
 </html>
